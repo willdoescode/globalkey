54312d10510581f46d2e64e9cb6a332b776401db
Will Lane <williamlane923@gmail.com>
2021-05-07 15:06:11 -0700

    Switched to more efficient way

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,16 @@
 use node_bindgen::derive::node_bindgen;
 use device_query::{DeviceState, DeviceQuery};
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 #[node_bindgen(mt)]
 fn start<F: Fn(Vec<String>) + Send + 'static>(callback: F) {
-	stoppable_thread::spawn(move |stop| {
+	std::thread::spawn(move|| {
     let state = DeviceState::new();
 
 		let mut previous_keys = Vec::new();
-		while !stop.get() {
+		loop {
 			let keys = state.get_keys();
 	    if !keys.is_empty() && keys != previous_keys {
-		    callback(keys.clone().into_par_iter().map(|key| format!("{}", key)).collect());
+		    callback(keys.clone().iter().map(|key| format!("{}", key)).collect());
 	    }
 	    previous_keys = keys;
     }
